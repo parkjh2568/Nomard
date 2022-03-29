6d2a8f10e0faad8d408beb7f9a60155efcc50918
junhyunpark <junhypar@gmail.com>
2022-03-29 11:53:01 +0900

    2022.03.29 toggle

--- VanilaJS/7cssinjs.js
+++ VanilaJS/7cssinjs.js
@@ -3,7 +3,10 @@ const h1 = document.querySelector("div.hello h1")
 console.dir(h1);
 const h2 = document.querySelector("div.hello h2")
 console.dir(h2);
-
+const h3 = document.querySelector("div.hello h3")
+console.dir(h3);
+const h4 = document.querySelector("div.hello h4")
+console.dir(h4);
 
 //인라인코드로 강제로 바꾸기
 function handleMouseClick() {
@@ -28,3 +31,25 @@ function handleMouseClick2() {
         h2.className = className
 }
 h2.addEventListener("click", handleMouseClick2)
+
+
+
+//css이용하기 -> classList 이용하기
+
+function handleMouseClick3() {
+    const className = "listactive"
+    if (h3.classList.contains(className))
+        h3.classList.remove(className)
+    else
+        h3.classList.add(className)
+}
+h3.addEventListener("click", handleMouseClick3)
+
+
+//css이용하기 -> classList.toggle 이용하기
+
+function handleMouseClick4() {
+    const className = "toggleactive"
+    h4.classList.toggle(className)
+}
+h4.addEventListener("click", handleMouseClick4)
